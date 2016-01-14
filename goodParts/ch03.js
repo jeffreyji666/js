2d7ce815a3aee6d3047c47a64d40a091066de199
jeffrey <jiwengang@qingmiaoer.com>
2016-01-14 19:14:03 +0800

    change from browser to node

--- goodParts/ch03.js
+++ goodParts/ch03.js
@@ -4,8 +4,8 @@ var stooge = {
     "first-name": "Jerome",
     "last-name": "Howard"
 };
-    
-    
+
+
 var flight = {
     airline: "Oceanic",
     number: 815,
@@ -20,98 +20,94 @@ var flight = {
         city: "Los Angeles"
     }
 };
-    
-    
+
+
 stooge["first-name"]     // "Joe"
 flight.departure.IATA    // "SYD"
-    
-    
+
+
 stooge["middle-name"]    // undefined
 flight.status            // undefined
 stooge["FIRST-NAME"]     // undefined
-    
-    
+
+
 var middle = stooge["middle-name"] || "(none)";
+console.log("middle:" + middle);
 var status = flight.status || "unknown";
-    
-    
-flight.equipment                              // undefined
-flight.equipment.model                        // throw "TypeError"
-flight.equipment && flight.equipment.model    // undefined
-    
-    
+console.log("status:" + status);
+
+
+console.log(flight.equipment)                            // undefined
+//flight.equipment.model                        // throw "TypeError"
+console.log(flight.equipment && flight.equipment.model)    // undefined
+
+
 stooge['first-name'] = 'Jerome';
-    
-    
+
+
 stooge['middle-name'] = 'Lester';
 stooge.nickname = 'Curly';
 flight.equipment = {
     model: 'Boeing 777'
 };
 flight.status = 'overdue';
-    
-    
+
+
 var x = stooge;
 x.nickname = 'Curly';
 var nick = stooge.nickname;
-    // nick is 'Curly' because x and stooge
-    // are references to the same object
+// nick is 'Curly' because x and stooge
+// are references to the same object
 
 var a = {}, b = {}, c = {};
-    // a, b, and c each refer to a
-    // different empty object
+// a, b, and c each refer to a
+// different empty object
 a = b = c = {};
-    // a, b, and c all refer to
-    // the same empty object
-    
-    
+// a, b, and c all refer to
+// the same empty object
+
+
 if (typeof Object.beget !== 'function') {
-     Object.beget = function (o) {
-         var F = function () {};
-         F.prototype = o;
-         return new F();
-     };
+    Object.beget = function (o) {
+        var F = function () {
+        };
+        F.prototype = o;
+        return new F();
+    };
 }
 var another_stooge = Object.beget(stooge);
-    
-    
+
 
 another_stooge['first-name'] = 'Harry';
 another_stooge['middle-name'] = 'Moses';
 another_stooge.nickname = 'Moe';
-    
-    
+
 
 stooge.profession = 'actor';
 another_stooge.profession    // 'actor'
-    
-    
+
 
 typeof flight.number      // 'number'
 typeof flight.status      // 'string'
 typeof flight.arrival     // 'object'
 typeof flight.manifest    // 'undefined'
-    
-    
+
 
 typeof flight.toString    // 'function'
 typeof flight.constructor // 'function'
-    
-    
+
 
 flight.hasOwnProperty('number')         // true
 flight.hasOwnProperty('constructor')    // false
-    
-    
+
 
 var name;
 for (name in another_stooge) {
     if (typeof another_stooge[name] !== 'function') {
-        document.writeln(name + ': ' + another_stooge[name]);
+        console.log(name + ': ' + another_stooge[name]);
     }
 }
-    
-    
+
 
 var i;
 var properties = [
@@ -121,12 +117,9 @@ var properties = [
     'profession'
 ];
 for (i = 0; i < properties.length; i += 1) {
-    document.writeln(properties[i] + ': ' +
-            another_stooge[properties[i]]);
-    }
+    console.log(properties[i] + ': ' + another_stooge[properties[i]]);
 }
-    
-    
+
 
 another_stooge.nickname    // 'Moe'
 
@@ -136,12 +129,11 @@ another_stooge.nickname    // 'Moe'
 delete another_stooge.nickname;
 
 another_stooge.nickname    // 'Curly'
-    
-    
+
 
 var MYAPP = {};
-    
-    
+
+
 MYAPP.stooge = {
     "first-name": "Joe",
     "last-name": "Howard"
--- goodParts/ch04.js
+++ goodParts/ch04.js
@@ -1,296 +1,298 @@
-var add = function(a, b) {
-	return a + b;
+var add = function (a, b) {
+    return a + b;
 };
 
 var myObject = {
-	value : 0,
-	increment : function(inc) {
-		print(typeof inc);
-		this.value += typeof inc === 'number' ? inc : 1;
-	}
+    value: 0,
+    increment: function (inc) {
+        console.log(typeof inc);
+        this.value += typeof inc === 'number' ? inc : 1;
+        console.log(this.value);
+    }
 };
 
 myObject.increment();
-print(myObject.value);
+console.log(myObject.value);
+console.log('-------');
 
 myObject.increment(2);
-print(myObject.value);
-print('-------');
+console.log(myObject.value);
+console.log('-------');
 
-myObject.double = function() {
-	this.value = add(this.value, this.value);
+myObject.double = function () {
+    this.value = add(this.value, this.value);
 }
 myObject.double();
-print(myObject.value);
+console.log(myObject.value);
 
-myObject.double = function() {
-	var that = this;
+myObject.double = function () {
+    var that = this;
 
-	var helper = function() {
-		that.value = add(that.value, that.value);
-	};
+    var helper = function () {
+        that.value = add(that.value, that.value);
+    };
 
-	helper();
+    helper();
 }
 
 myObject.double();
-print(myObject.value);
-print('-------');
+console.log(myObject.value);
+console.log('-------');
 
-var Quo = function(string) {
-	this.status = string;
+var Quo = function (string) {
+    this.status = string;
 };
 
-Quo.prototype.get_status = function() {
-	return this.status;
+Quo.prototype.get_status = function () {
+    return this.status;
 };
 
 var myQuo = new Quo("confused");
-print(myQuo.get_status())
-print('-------');
+console.log(myQuo.get_status())
+console.log('-------');
 
-var array = [ 3, 4 ];
+var array = [3, 4];
 var sum = add.apply(null, array);
-print(sum);
+console.log(sum);
 
 var statusObject = {
-	status : 'A-OK'
+    status: 'A-OK'
 };
 
 // var status = Quo.prototype.get_status().apply(statusObject);
 var status = Quo.prototype.get_status.apply(statusObject);
-print(status);
-print('-------');
+console.log(status);
+console.log('-------');
 
-var sum = function() {
-	var sum = 0;
-	for (var i = 0; i < arguments.length; i += 1) {
-		sum += arguments[i];
-	}
-	return sum;
+var sum = function () {
+    var sum = 0;
+    for (var i = 0; i < arguments.length; i += 1) {
+        sum += arguments[i];
+    }
+    return sum;
 };
-print(sum(4, 8, 15, 112, 34234, 13412, 43));
-print('-------');
+console.log(sum(4, 8, 15, 112, 34234, 13412, 43));
+console.log('-------');
 
-var add = function(a, b) {
-	if (typeof a !== 'number' || typeof b !== 'number') {
-		throw {
-			name : 'TypeError',
-			message : 'add needs numbers'
-		};
-	}
-	return a + b;
+var add = function (a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw {
+            name: 'TypeError',
+            message: 'add needs numbers'
+        };
+    }
+    return a + b;
 }
 
-var try_it = function() {
-	try {
-		add("seven");
-	} catch (e) {
-		print(e.name + ":" + e.message)
-	}
+var try_it = function () {
+    try {
+        add("seven");
+    } catch (e) {
+        console.log(e.name + ":" + e.message)
+    }
 }
 
 try_it();
-print('-------');
+console.log('-------');
 
-Function.prototype.method = function(name, func) {
-	if (!this.prototype[name]) {
-		this.prototype[name] = func;
-	}
-	return this;
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+    }
+    return this;
 };
 
-Number.method('integer', function() {
-	return Math[this < 0 ? 'ceil' : 'floor'](this);
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
 });
 
-// print((-10 / 3).integer);
-print((-10 / 3).integer());
-print(Math.ceil(-10 / 3));
+// console.log((-10 / 3).integer);
+console.log((-10 / 3).integer());
+console.log(Math.ceil(-10 / 3));
 
-String.method('trim', function() {
-	return this.replace(/^\s+|\s+$/g, '');
+String.method('trim', function () {
+    return this.replace(/^\s+|\s+$/g, '');
 });
 
-print('"' + "   neat   ".trim() + '"');
-print('-------');
+console.log('"' + "   neat   ".trim() + '"');
+console.log('-------');
 
-var hanoi = function(disc, src, aux, dst) {
-	if (disc > 0) {
-		hanoi(disc - 1, src, dst, aux);
-		print('Move disc ' + disc + ' from ' + src + ' to ' + dst);
-		hanoi(disc - 1, aux, src, dst);
-	}
+var hanoi = function (disc, src, aux, dst) {
+    if (disc > 0) {
+        hanoi(disc - 1, src, dst, aux);
+        console.log('Move disc ' + disc + ' from ' + src + ' to ' + dst);
+        hanoi(disc - 1, aux, src, dst);
+    }
 };
 
 hanoi(3, 'Src', 'Aux', 'Dst');
-print('-------');
+console.log('-------');
 
 var factorial = function factorial(i, a) {
-	a = a || 1;
-	if (i < 2) {
-		return a;
-	}
-	return factorial(i - 1, a * i);
+    a = a || 1;
+    if (i < 2) {
+        return a;
+    }
+    return factorial(i - 1, a * i);
 }
 
-print(factorial(4));
-print('-------');
+console.log(factorial(4));
+console.log('-------');
 
-var myObject = (function() {
-	var value = 0;
+var myObject = (function () {
+    var value = 0;
 
-	return {
-		increment : function(inc) {
-			value += typeof inc === 'number' ? inc : 1;
-		},
-		getValue : function() {
-			return value;
-		}
-	};
+    return {
+        increment: function (inc) {
+            value += typeof inc === 'number' ? inc : 1;
+        },
+        getValue: function () {
+            return value;
+        }
+    };
 }());
 
 myObject.increment(20);
-print(myObject.getValue());
-print('-------');
+console.log(myObject.getValue());
+console.log('-------');
 
-var quo = function(status) {
-	return {
-		get_status : function() {
-			return status;
-		}
-	};
+var quo = function (status) {
+    return {
+        get_status: function () {
+            return status;
+        }
+    };
 };
 
 var myQuo = quo("amazed");
-print(myQuo.get_status());
-print('-------');
+console.log(myQuo.get_status());
+console.log('-------');
 
-var fade = function(node) {
-	var level = 1;
-	var step = function() {
-		var hex = level.toString(16);
-		node += '#FFFF' + hex + hex;
-		print(node);
-		if (level < 15) {
-			level += 1;
-			step();
-		}
-	};
-	step();
+var fade = function (node) {
+    var level = 1;
+    var step = function () {
+        var hex = level.toString(16);
+        node += '#FFFF' + hex + hex;
+        console.log(node);
+        if (level < 15) {
+            level += 1;
+            step();
+        }
+    };
+    step();
 };
 fade('test');
-print('-------');
+console.log('-------');
 
-var add_the_handlers = function(nodes) {
-	for (var i = 0; i < nodes.length; i += 1) {
-		nodes[i].onclick = function(e) {
-			print(i);
-		};
-	}
+var add_the_handlers = function (nodes) {
+    for (var i = 0; i < nodes.length; i += 1) {
+        nodes[i].onclick = function (e) {
+            console.log(i);
+        };
+    }
 };
 
-var add_the_handlers = function(nodes) {
-	var helper = function(i) {
-		return function(e) {
-			print(i);
-		};
-	};
-	for (var i = 0; i < nodes.length; i += 1) {
-		nodes[i].onclick = helper(i);
-	}
+var add_the_handlers = function (nodes) {
+    var helper = function (i) {
+        return function (e) {
+            console.log(i);
+        };
+    };
+    for (var i = 0; i < nodes.length; i += 1) {
+        nodes[i].onclick = helper(i);
+    }
 };
 
-String.method('deentityify', function() {
-	var entity = {
-		quot : '"',
-		lt : '<',
-		gt : '>'
-	};
-	return function() {
-		return this.replace(/&([^&;]+);/g, function(a, b) {
-			print(a + "::" + b);
-			var r = entity[b];
-			return typeof r === 'string' ? r : a;
-		});
-	};
+String.method('deentityify', function () {
+    var entity = {
+        quot: '"',
+        lt: '<',
+        gt: '>'
+    };
+    return function () {
+        return this.replace(/&([^&;]+);/g, function (a, b) {
+            console.log(a + "::" + b);
+            var r = entity[b];
+            return typeof r === 'string' ? r : a;
+        });
+    };
 }());
-print('&lt;&quot;&gt;'.deentityify());
-print('-------');
+console.log('&lt;&quot;&gt;'.deentityify());
+console.log('-------');
 
-var serial_maker = function() {
-	var prefix = '';
-	var seq = 0;
-	return {
-		set_prefix : function(p) {
-			prefix = String(p);
-		},
-		set_seq : function(s) {
-			seq = s;
-		},
-		gensym : function() {
-			var result = prefix + seq;
-			seq += 1;
-			return result;
-		}
-	};
+var serial_maker = function () {
+    var prefix = '';
+    var seq = 0;
+    return {
+        set_prefix: function (p) {
+            prefix = String(p);
+        },
+        set_seq: function (s) {
+            seq = s;
+        },
+        gensym: function () {
+            var result = prefix + seq;
+            seq += 1;
+            return result;
+        }
+    };
 };
 
 var seqer = serial_maker();
 seqer.set_prefix('Q');
 seqer.set_seq(10000);
 var unique = seqer.gensym();
-print(unique);
-print('-------');
+console.log(unique);
+console.log('-------');
 
-Function.method('curry', function() {
-	var slice = Array.prototype.slice;
-	var args = slice.apply(arguments);
-	var that = this;
+Function.method('curry', function () {
+    var slice = Array.prototype.slice;
+    var args = slice.apply(arguments);
+    var that = this;
 
-	return function() {
-		return that.apply(null, args.concat(slice.apply(arguments)));
-	};
+    return function () {
+        return that.apply(null, args.concat(slice.apply(arguments)));
+    };
 });
 
 var add1 = add.curry(1);
-print(add1(6));
-print('-------');
+console.log(add1(6));
+console.log('-------');
 
-var fibonacci = function() {
-	var memo = [ 0, 1 ];
-	var fib = function(n) {
-		var result = memo[n];
-		if (typeof result !== 'number') {
-			result = fib(n - 1) + fib(n - 2);
-			memo[n] = result;
-		}
-		return result;
-	};
-	return fib;
+var fibonacci = function () {
+    var memo = [0, 1];
+    var fib = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = fib(n - 1) + fib(n - 2);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return fib;
 }();
-print(fibonacci(10));
-print('-------');
+console.log(fibonacci(10));
+console.log('-------');
 
-var memorizer = function(memo, formula) {
-	var recur = function(n) {
-		var result = memo[n];
-		if (typeof result !== 'number') {
-			result = formula(recur, n);
-			memo[n] = result;
-		}
-		return result;
-	};
-	return recur;
+var memorizer = function (memo, formula) {
+    var recur = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = formula(recur, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return recur;
 };
-var fibonacci = memorizer([ 0, 1 ], function(recur, n) {
-	return recur(n - 1) + recur(n - 2);
+var fibonacci = memorizer([0, 1], function (recur, n) {
+    return recur(n - 1) + recur(n - 2);
 });
-print(fibonacci(10));
-print('-------');
+console.log(fibonacci(10));
+console.log('-------');
 
-var factorial = memorizer([ 1, 1 ], function(recur, n) {
-	return n * recur(n - 1);
+var factorial = memorizer([1, 1], function (recur, n) {
+    return n * recur(n - 1);
 });
-print(factorial(4));
-print('-------');
+console.log(factorial(4));
+console.log('-------');
